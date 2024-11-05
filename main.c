057d8311aeb5d7e356f59b0d6d351df1b7542770
leomuso <Pinosenx@gmail.com>
2024-11-05 18:47:43 +0100

    exercise 4.36

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int salti = 0;
+    int cm;
+    printf("inserisci i cm\n");
+    scanf("%d", &cm);
+    while (cm != 1 && cm != 0) {
+        salti++;
+        cm = cm * 0.8;
+        printf("%d\n", cm);
+    }
+    printf("il numero di salti e' %d", salti);
+}
